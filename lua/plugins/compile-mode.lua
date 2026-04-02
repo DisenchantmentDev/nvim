5ae0607e52b204d8c3863d173c9f4c431f8ea113
DisenchantmentDev <collinmbarnett@gmail.com>
2026-04-02 11:34:58 -0600

    compile module

---
+++ lua/plugins/compile-mode.lua
@@ -0,0 +1,13 @@
+return {
+  "ej-shafran/compile-mode.nvim",
+  version = "^5.0.0",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    ---@type CompileModeOpts
+    vim.g.compile_mode = {
+      input_word_completion = true,
+    }
+  end,
+}
